ec469bbd4d7e194ff017bf2a3f2d66c5071c7af6
Talha Kösen <talhakosen@hotmail.com>
2012-12-09 19:06:10 +0200

    Rev1

--- src/com/example/progressbar/ProgressBarActivity.java
+++ src/com/example/progressbar/ProgressBarActivity.java
@@ -3,21 +3,22 @@ package com.example.progressbar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class ProgressBarActivity extends Activity {
 
 	Button btnStartProgress;
-	SeekBar seekBar;
+	SeekBar progressBar;
+	ProgressBar p;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.progresbar_main);
-		seekBar = (SeekBar) findViewById(R.id.seekBar2);
-		
-		seekBar.setProgressDrawable(getResources().getDrawable(R.drawable.seekbar));
+		progressBar = (SeekBar) findViewById(R.id.progressBar);		
+		progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.seekbar));
 		
 
 	}
